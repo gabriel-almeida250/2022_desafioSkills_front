5c07288cbd9cd2a0b71553b4b2f1bbf0f5f00b34
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-08-28 00:35:48 -0300

    Pequenas atualizações

--- desafio-skills/src/pages/Home/index.jsx
+++ desafio-skills/src/pages/Home/index.jsx
@@ -1,19 +1,19 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import ModalUsuarioSkillPost from "../../components/Modals/ModalUsuarioSkillPost";
 import add from "../../assets/img/icon-add.png";
-import "./styles.css";
-import { API } from "../../services/api";
-import HeaderPrivado from "../../components/HeaderPrivado";
 import UpdateDelete from "../../components/Acoes/UpdateDelete";
+import HeaderPrivado from "../../components/HeaderPrivado";
+import ModalUsuarioSkillPost from "../../components/Modals/ModalUsuarioSkillPost";
+import { API } from "../../services/api";
+import "./styles.css";
 
 const Home = () => {
 
-const [user, setUser] = useState('');
-const usuarioLogado = localStorage.getItem('login')
+  const [user, setUser] = useState('');
 
-useEffect(() => {
+  useEffect(() => {
+    const usuarioLogado = localStorage.getItem('login')
     const pegarDados = async () => {
       await API.get(`/api/usuario/usuarioAchado/${JSON.parse(usuarioLogado)}`)
         .then((response) => setUser(response.data))
@@ -22,50 +22,49 @@ useEffect(() => {
         });
     };
     pegarDados();
-  }, []); 
-
-  console.log("here "+user.id);
+  }, []);
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const abrirModal = () => {
-      handleShow()
+    handleShow()
   }
 
-
-return (
-  <>
-  <HeaderPrivado />
-   <div className="adicionarSkill" onClick={() => abrirModal()} title='Adicionar Skill'>
-                    <img src={add} />
-                </div>
-                <ModalUsuarioSkillPost show={show} handleClose={handleClose} idUsuario={user.id}/>
-    <div className="card" style={{ borderBottomWidth: "0", borderTopWidth: "0"}}>
-      {user.usuarioSkill !== undefined && (
-        <Container>
-        <div className="container-produto row" >
-          {user.usuarioSkill?.map((skill, index) => (
-            <Card className="col-lg-auto" key={index} style={{ maxWidth: "200px"}}>
-              <Card.Img className="imagem-card"
-                variant="top"
-                src={skill.skill.imageUrl.toString()}
-                alt={skill.skill.description}
-              />
-              <Card.Body>
-                <Card.Title>{skill.skill.name}</Card.Title>
-                <Card.Text>{skill.skill.versao}</Card.Text>
-                <Card.Text>{skill.skill.description}</Card.Text>
-                <Card.Text>{skill.knowledgeLevel}</Card.Text>
-                <UpdateDelete identificador={skill.usuarioSkillId} nomeDeletado={skill.skill.name} identificadorId={skill.skill.idSkill} />
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
-        </Container>
-      )}
-    </div>
+  return (
+    <>
+      <HeaderPrivado />
+      <div className="adicionarSkill" onClick={() => abrirModal()} title='Adicionar Skill'>
+        <img src={add} />
+      </div>
+      <ModalUsuarioSkillPost show={show} handleClose={handleClose} idUsuario={user.id} />
+      <div className="card" style={{ borderBottomWidth: "0", borderTopWidth: "0" }}>
+        {user.usuarioSkill !== undefined && (
+          <Container>
+            <div className="container-produto row" >
+              {user.usuarioSkill?.sort(function (a, b) {
+                return (a.skill.name > b.skill.name) ? 1 : ((b.skill.name > a.skill.name) ? -1 : 0);
+              }).map((skill, index) => (
+                <Card className="col-lg-auto" key={index} style={{ maxWidth: "200px" }}>
+                  <Card.Img className="imagem-card"
+                    variant="top"
+                    src={skill.skill.imageUrl.toString()}
+                    alt={skill.skill.description}
+                  />
+                  <Card.Body>
+                    <Card.Title>{skill.skill.name}</Card.Title>
+                    <Card.Text>{skill.skill.versao}</Card.Text>
+                    <Card.Text>{skill.skill.description}</Card.Text>
+                    <Card.Text>{skill.knowledgeLevel}</Card.Text>
+                    <UpdateDelete identificador={skill.usuarioSkillId} nomeDeletado={skill.skill.name} identificadorId={skill.skill.idSkill} />
+                  </Card.Body>
+                </Card>
+              ))}
+            </div>
+          </Container>
+        )}
+      </div>
     </>
   );
 };
